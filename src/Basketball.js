08128e73dbf4a0ee1603594f6a0a9a4a45c7506a
Mekel Tensley <mekelintech@gmail.com>
2021-12-15 18:42:46 -0500

    Initial Commit

---
+++ src/Basketball.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Basketball extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Basketball</h1>
+            </div>
+        )
+    }
+}
+
+export default Basketball;
---
+++ src/Racing.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+const displayDrivers = payload,map((driver, idx) => {
+    return (
+        <div>
+        </div>
+    )
+})
+
+class Racing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Racing</h1>
+                <div>
+                    <h1>{driver.name}</h1>
+                    <p>{driver.team}</p>
+                </div>
+                {displayDrivers}
+            </div>
+        );
+    }
+}
+
+export default Racing;
