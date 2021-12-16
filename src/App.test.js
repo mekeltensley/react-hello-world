1ca553608451d4272ff147a8304c094855befbb9
Mekel Tensley <mekelintech@gmail.com>
2021-12-16 16:50:21 -0500

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Forum from './Forum';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders dashboard tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Dasboard/i);
+  expect(spanElement).toBeInTheDocument();
 });
+
+test('renders customers tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Customers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders authentication tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Authentication/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders payments tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders transfers tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Transfer/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders question tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByTitle(/Question/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
