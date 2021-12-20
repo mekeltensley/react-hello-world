b130789c75fe5f45b3c729fb3fa767445e07f872
Mekel Tensley <mekelintech@gmail.com>
2021-12-20 16:41:36 -0500

    Update array of objects

--- src/App.js
+++ src/App.js
@@ -12,10 +12,48 @@ import Personal from './Personal';
 import Cheatsheet from './Cheatsheet';
 import Article from './Article';
 
+const driver = {
+  name: 'Lewis Hamiliton',
+  team: 'Mercedes',
+}
+
+const payload = [
+  {
+    name: 'Driver 2',
+    team: 'Team 2'
+  },
+  {
+    name: 'Driver 3',
+    team: 'Team 3'
+  },
+  {
+    name: 'Driver 4',
+    team: 'Team 4'
+  },
+  {
+    name: 'Driver 5',
+    team: 'Team 5'
+  },
+]
+
+const displayDrivers = payload.map((driver, idx) => {
+  return (
+    <div key={idx}>
+      <h1>Name: {driver.name}</h1>
+      <p>Team: {driver.team}</p>
+    </div>
+  );
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Driver {driver.name}</h1>
+        <p>Team {driver.team}</p>
+        <div>
+
+        </div>
         <Personal 
         title="I'm Mekel"
         subtitle="A Creative and Full Stack Developer"
